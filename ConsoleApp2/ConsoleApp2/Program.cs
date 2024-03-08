cdc31b613afaa7d149d0e97b27e3e0a145ac86c4
January24844 <s24844@pjwstk.edu.pl>
2024-03-08 09:49:07 +0100

    modyfikacja2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+int []tab = { 1, 2, 3, 4, 5, 6 };
+Console.WriteLine(getAvg(tab));
+
+return;
+
+static double getAvg(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum / arr.Length;
+}
