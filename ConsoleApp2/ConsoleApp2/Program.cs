ed1ccfbd89481fe1ca4853e9ef5bef239ad95f6e
janek.wilkowski@gmail.com <janek.wilkowski@gmail.com>
2024-03-14 14:04:17 +0100

    f-m commit

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Hello, World!");
 
 int []tab = { 1, 2, 3, 4, 5, 6 };
 Console.WriteLine(getAvg(tab));
-
+Console.WriteLine(getMax(tab));
 return;
 
 static double getAvg(int[] arr)
@@ -16,4 +16,16 @@ static double getAvg(int[] arr)
     }
 
     return sum / arr.Length;
+}
+static int getMax(int[]arr)
+{
+	int max = 0;
+	for	(int i = 0; i < arr.Length; i++)
+	{
+		if	(arr[i]>max)
+		{
+			max = arr[i];
+		}
+	}
+	return max;
 }
