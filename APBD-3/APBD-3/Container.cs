7c80a3f04ebfbf57fede41219614905f800fef3a
janek.wilkowski@gmail.com <janek.wilkowski@gmail.com>
2024-03-23 17:27:34 +0100

    got my abstract container, a good start.

---
+++ APBD-3/APBD-3/Container.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace APBD_3
+
+{
+    public abstract class Container
+    {
+        public int LoadCapacity { get; protected set; }
+        public int Height { get; protected set; }
+        public int Weight { get; protected set; }
+        public int Depth { get; protected set; }
+        public string SerialNumber { get; protected set; }
+
+        public Container(int loadCapacity, int height, int weight, int depth, string serialNumber)
+        {
+            LoadCapacity = loadCapacity;
+            Height = height;
+            Weight = weight;
+            Depth = depth;
+            SerialNumber = serialNumber;
+        }
+        public abstract void Load(int cargoWeight);
+
+        public abstract void Unload();
+    }
+}
