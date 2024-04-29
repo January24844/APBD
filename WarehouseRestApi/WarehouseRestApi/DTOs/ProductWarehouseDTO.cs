e7c44f9cefb75ddf117457950108425978332359
January <janek.wilkowski@gmail.com>
2024-04-29 17:16:39 +0200

    Ado.Net approach

---
+++ WarehouseRestApi/WarehouseRestApi/DTOs/ProductWarehouseDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WarehouseRestApi.DTOs;
+
+public record Order(
+    [Required] int IdProduct,
+    [Required] int IdWarehouse,
+    [Required] int Amount,
+    [Required] DateTime CreatedAt
+);
